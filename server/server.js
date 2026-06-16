0be80bd408cdafb4c3c23bc9e54332469c2019e9
SarthakDudhe <opharry29@gmail.com>
2026-06-16 13:08:05 +0530

    feat: implement socket.io server with real-time messaging features and backend environment configuration

--- server/server.js
+++ server/server.js
@@ -97,8 +97,10 @@ app.use("/api/conversations",conversationRouter)
 
 await connectDB();
 
-const PORT=process.env.PORT || 5000;
-server.listen(PORT,()=>console.log("Server is running on PORT : ",PORT));
+if (!process.env.VERCEL) {
+  const PORT=process.env.PORT || 5000;
+  server.listen(PORT,()=>console.log("Server is running on PORT : ",PORT));
+}
 
 //Export server for vercel
 export default server;
